f582523bf3a1f37ca9410c310d3a4cc3e8667c25
Julia Y <Julia@MacBook-Pro-7.local>
2021-03-25 19:07:05 -0400

    final

--- diss_9.py
+++ diss_9.py
@@ -6,42 +6,50 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    anchor = soup.find('div', class_='infocard-list infocard-list-pkmn-lg')
+    anchor2 = anchor.find_all('span',  class_='infocard-lg-img')[3]
+    anchor3 = anchor2.find('a')['href']
+    return('https://pokemondb.net' + anchor3)
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
+    moves = []
     url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
+    soup = BeautifulSoup((requests.get(url)).text, 'html.parser')
+    l = soup.find(text = 'Egg moves').findNext('table')
+    m = l.find_all('a', class_ = 'ent-name')
+    for i in m:
+        moves.append(i.get_text())
+    return(moves)
 
+
+
+
+    
+
+    
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    lst = []
 
     # define the regular expression
-    
+    reg = '@(\d{1,2} ?(?:pm|am))'
 
     # loop through each sentence or phrase in sentences
-    
-
-    # find all the words that match the regular expression in each sentence
-       
-
-    # loop through the found words and add the words to your empty list
-
-
-    #return the list of the last letter of all words that begin or end with a capital letter
-
+    for i in sentences:
+        temp = re.findall(reg, i)
+        for j in temp:
+            lst.append(j)
+    return(lst)
 
 
 def main():
     url = 'https://pokemondb.net/pokedex/national'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    getCharmanderLink(soup)
-    getEggMoves('scizor')
 
 class TestAllMethods(unittest.TestCase):
     def setUp(self):
@@ -60,3 +68,4 @@ class TestAllMethods(unittest.TestCase):
 if __name__ == "__main__":
     main()
     unittest.main(verbosity = 2)
+
